49f1ec86f007e55d4ee2b3ada0ac5708031cf84f
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-09-13 16:22:19 +0200

    [mod-4] Build form model

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 import { Customer } from './customer';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+    selector: 'app-customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  customer = new Customer();
 
-  constructor() { }
+    customerForm: FormGroup;
+    customer = new Customer();
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
-  }
+    ngOnInit(): void {
+        this.customerForm = new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            sendCatalog: new FormControl(true),
+        });
+    }
+
+    save(): void {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
 }

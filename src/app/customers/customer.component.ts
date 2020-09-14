a506e11179e8dab55fd64c0c33b27bcbd048c697
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-09-14 22:36:34 +0200

    [mod-6] Adjusting validation rules

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -52,6 +52,10 @@ export class CustomerComponent implements OnInit {
             rating: [null, ratingRange(1, 5)],
             sendCatalog: true
         });
+
+        this.customerForm.get('notification').valueChanges.subscribe(
+            value => this.setNotification(value)
+        );
     }
 
     populatePartial(): void {
